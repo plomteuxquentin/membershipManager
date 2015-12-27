4086d85833701bf38d704761547289e85d1ac58e
Quentin Plomteux <plomteuxquentin@gmail.com>
2015-12-27 19:06:28 +0100

    journal timeline

--- src/client/app/journal/journal.controller.js
+++ src/client/app/journal/journal.controller.js
@@ -9,10 +9,55 @@
   /* @ngInject */
   function JournalController() {
     var self = this;
+    self.events = [];
 
     activate();
 
     function activate() {
+      self.events = [
+        {
+          id: 1,
+          date: new Date('09/19/2014'),
+          icon: 'addMember',
+          title: 'Kamille Subscription'
+        },
+        {
+          id: 2,
+          date: new Date('09/26/2014'),
+          icon: 'addSeance',
+          title: 'Kamille bought 10 seances'
+        },
+        {
+          id: 3,
+          date: new Date('09/27/2014'),
+          icon: 'addSession',
+          title: 'Session of close-combat'
+        },
+        {
+          id: 3,
+          date: new Date('09/27/2014'),
+          icon: 'addSession',
+          title: 'Session of close-combat'
+        },
+        {
+          id: 3,
+          date: new Date('09/27/2014'),
+          icon: 'addSession',
+          title: 'Session of close-combat'
+        },
+        {
+          id: 3,
+          date: new Date('09/27/2014'),
+          icon: 'addSession',
+          title: 'Session of close-combat'
+        },
+        {
+          id: 3,
+          date: new Date('09/27/2014'),
+          icon: 'addSession',
+          title: 'Session of close-combat'
+        }
+      ];
     }
   }
 })();
