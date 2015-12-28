03970ac5210f1ab66e8fe1d1dd3e54e304378034
Quentin Plomteux <plomteuxquentin@gmail.com>
2015-12-28 15:37:21 +0100

    member form

--- src/client/app/members/edit/members.edit.controller.js
+++ src/client/app/members/edit/members.edit.controller.js
@@ -5,14 +5,21 @@
     .module('app.members.edit')
     .controller('MembersEditController', MembersEditController);
 
-  MembersEditController.$inject = [];
+  MembersEditController.$inject = ['$scope', '$mdBottomSheet'];
   /* @ngInject */
   function MembersEditController() {
     var self = this;
+    self.addImage = addImage;
 
     activate();
 
-    function activate() {
+    function activate () {
+    }
+
+    function addImage () {
+      console.debug('log');
+
+      document.getElementById('capturePhoto').click();
     }
   }
 })();
